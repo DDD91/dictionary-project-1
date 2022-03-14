8be8fbc47c775507e8bf5fa1c43ee841a5d1242b
DDD91 <ghecci1614@gmail.com>
2022-03-14 09:22:50 -0400

    Added image & styled CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import dictionary from "./dictionary.jpg";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-        <header className="App-header"></header>
+        <header className="App-header">
+        <img src={dictionary} className="App-logo img-fluid"
+        alt="logo" />  
+        </header>
     </div>
   );
 }
