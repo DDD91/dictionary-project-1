c310243ab6fcef5eaf272b7db4e0c91179886589
DDD91 <ghecci1614@gmail.com>
2022-03-14 13:47:03 -0400

    Created two components for results & meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,14 @@
 import React, { useState }  from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
-
+    let [results, setResults] = useState(null);
 
     function handleResponse(response) {
-        console.log(response.data[0]);
+        setResults(response.data[0]);
     }
 
     function search(event) {
@@ -32,6 +33,7 @@ return (
       onChange= {handleKeywordChange} 
       />
   </form>
+  <Results results={results}/>
 </div>
  );
 }
