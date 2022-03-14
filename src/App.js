b354df14b0964c0870ec94063c22614a198a2821
DDD91 <ghecci1614@gmail.com>
2022-03-14 11:34:37 -0400

    Added form for search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import dictionary from "./dictionary.jpg";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
         <header className="App-header">
         <img src={dictionary} className="App-logo img-fluid"
         alt="logo" />  
         </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Devanie Dawson</small>        </footer>
+      </div>
     </div>
   );
 }
