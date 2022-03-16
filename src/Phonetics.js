d516c1f04beae3cc9a887b254d3564715117cb0b
DDD91 <ghecci1614@gmail.com>
2022-03-16 18:08:54 -0400

    edit

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Phonectics(props) {
  return (
      <div className="Phonetics">
-         <a href={props.phonetic.audio} target="_blank">
+         <a href={props.phonetic.audio} target="_blank" rel="noopener">
              Listen
          </a>
          <br />
