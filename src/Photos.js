9f2b3a8c3d07f62b5a0c7db9ca207af14184c171
DDD91 <ghecci1614@gmail.com>
2022-03-18 20:03:42 -0400

    Update Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,7 @@ return (
           return (
           <div className="col-4" key={index}>
           <a href={photo.src.orginal} target="_blank" rel="noreferrer">
-          <img src={photo.src.landscape} alt="photo"
+          <img src={photo.src.landscape} alt=""
           className="img-fluid"  />
           </a>
           </div>
